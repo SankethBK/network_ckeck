5b9b152ff629cccb9ed2ec42724c8908d43c4071
sankethbk7777 <sankethbk7777@gmail.com>
2021-01-03 08:13:43 +0530

    Updated network check

--- netowrk_ckeck.py
+++ netowrk_ckeck.py
@@ -1,7 +1,9 @@
 import socket
 
-try:
-	socket.gethostbyname("www.google.com")
-except:
-	print("No network")
-		
+def network_check():
+	try:
+		socket.gethostbyname("www.google.com")
+	except:
+		print("No network")
+network_check()
+			
